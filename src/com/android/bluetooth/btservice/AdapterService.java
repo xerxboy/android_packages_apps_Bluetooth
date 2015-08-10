32a3ab8358b48ce233802711c0b9ca07ed2d93e8
Gaurav Asati <gasati@codeaurora.org>
2015-08-10 12:51:20 +0530

    Add device in Delay Connect Time-out list
     - Bose headset does not respond to AFH packets
       when send from DUT.Due to this A2dp connection fails.
       Since we are already trying to connect A2dp, 6 sec
       connection re-try mechanism also fails.
       Add Bose device for 10 sec delay time-out for connection
       re-try
    
    Change-Id: Ibf0bbfed0eff5fde1a590513039789f9ea6aa5e7

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -97,6 +97,7 @@ public class AdapterService extends Service {
     private static final String delayConnectTimeoutDevice[] =
             {"00:23:3D", // volkswagen carkit
              "00:23:01", // Roman 9020 HS
+             "00:0C:8A", // Bose HS
              "00:0C:36"};// Bose series 2 HS
     //For Debugging only
     private static int sRefCount=0;
